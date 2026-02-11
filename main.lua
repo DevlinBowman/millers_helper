323a6c0a68f5bd3d6eb98df9faedcbc201bda675
DevlinBowman <id+DevlinBowman@users.noreply.github.com>
2026-02-11 12:31:36 -0800

    clean state (kind of) moving to attempt to properly re-delegate ingestion layer responsibilities

--- main.lua
+++ main.lua
@@ -1,14 +1,6 @@
 -- main.lua
 --
 local I           = require("inspector")
-local Adapter     = require("ingestion.adapter")
-local Report      = require("ingestion.report")
-
-local Ledger      = require("core.ledger")
-
--- local INPUT       = "tests/data_format/old_sheet.csv"
--- local INPUT       = "tests/data_format/input.txt":w
---
 local test_inputs = {
     -- '/Users/ven/Desktop/2026-lumber-app-v2/data/test_inputs/input.txt',
     '/Users/ven/Desktop/2026-lumber-app-v2/data/test_inputs/test_lumber.csv',
@@ -16,13 +8,9 @@ local test_inputs = {
     -- '/Users/ven/Desktop/2026-lumber-app-v2/data/test_inputs/test_lumber.txt'
 }
 
-local IO          = require('io.controller')
+local IO = require("application.runtime.io")
 
+local result = IO.read_strict("data/test_inputs/test_lumber.json")
+I.print(result, {shape_only = true})
 
-for i, file in pairs(test_inputs) do
-    print(file)
-    -- local data = IO.read(file)
-    local data = Adapter.ingest(file)
-    I.print(data, {shape_only = true})
-end
 
