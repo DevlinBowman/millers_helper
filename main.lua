678260e0db72ebe69a7730271cb160e754f60850
DevlinBowman <id+DevlinBowman@users.noreply.github.com>
2026-02-13 20:14:22 -0800

    working ingestion in pipeline

--- main.lua
+++ main.lua
@@ -6,20 +6,25 @@ local Format = require('format.controller')
 local Trace = require('tools.trace')
 Trace.set(true)
 
--- 1. Read raw codec envelope
-local raw = IO.read('/Users/ven/Desktop/2026-lumber-app-v3/data/test_inputs/test_lumber.json')
-print('raw')
--- I.print(raw, { shape_only = true })
-
--- 2. Decode to canonical objects
+-- -- 1. Read raw codec envelope
+-- local raw = IO.read('/Users/ven/Desktop/2026-lumber-app-v3/data/test_inputs/test_lumber.json')
+-- print('raw')
+-- -- I.print(raw, { shape_only = true })
 --
-local decoded = Format.decode(raw.codec, raw.data)
-I.print(decoded, { shape_only = true })
-
--- 3. Encode objects to target codec
-local encoded = Format.encode("lua", decoded.data)
-I.print(encoded, { shape_only = true })
-
--- 4. Write codec envelope
-local out = IO.write('data/out/json_to_lua.lua', encoded)
-I.print(out)
+-- -- 2. Decode to canonical objects
+-- --
+-- local decoded = Format.decode(raw.codec, raw.data)
+-- I.print(decoded, { shape_only = true })
+--
+-- -- 3. Encode objects to target codec
+-- local encoded = Format.encode("lua", decoded.data)
+-- I.print(encoded, { shape_only = true })
+--
+-- -- 4. Write codec envelope
+-- local out = IO.write('data/out/json_to_lua.lua', encoded)
+-- I.print(out)
+--
+local Ingest = require('pipelines.ingest')
+local test_text = 'notes = szsdf'
+local txt_rd = Ingest.read('/Users/ven/Desktop/2026-lumber-app-v3/data/test_inputs/no_boards.txt')
+I.print(txt_rd)
